5401c11bddbe3266cbfbcd694bddd610ac74a343
PuTongGithub <282549045@qq.com>
2017-07-24 18:08:27 +0800

    项目文档

---
+++ 第五组/公共文件/pms2.0.sql
@@ -0,0 +1,263 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for Win32 (ia32)
+--
+-- Host: localhost    Database: pms_database
+-- ------------------------------------------------------
+-- Server version	5.1.73-community
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `mark` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'dep_test','00001','using');
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `sex` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `birthday` varchar(255) NOT NULL,
+  `id_number` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `entry_date` varchar(255) NOT NULL,
+  `work_date` varchar(255) NOT NULL,
+  `employment_form` varchar(255) NOT NULL,
+  `personnel_source` varchar(255) NOT NULL,
+  `statu` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'aaa','男','1','1990-01-01','12345','人事部','经理','2010-12-10','2008-07-30','123','123','123'),(2,'bbb','女','2','1992-08-15','54321','技术部','后台开发','2017-01-02','2017-01-02','123','123','123'),(3,'ccc','男','3','1995-11-21','13467','销售部','销售员','2017-03-20','2017-03-20','123','123','123');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `marriage` varchar(255) DEFAULT NULL,
+  `folk` varchar(255) DEFAULT NULL,
+  `political` varchar(255) DEFAULT NULL,
+  `education` varchar(255) DEFAULT NULL,
+  `school` varchar(255) DEFAULT NULL,
+  `major` varchar(255) DEFAULT NULL,
+  `graduate_date` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `positions`
+--
+
+DROP TABLE IF EXISTS `positions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `mark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `positions`
+--
+
+LOCK TABLES `positions` WRITE;
+/*!40000 ALTER TABLE `positions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `positions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resign`
+--
+
+DROP TABLE IF EXISTS `resign`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resign` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `begin_date` varchar(255) NOT NULL,
+  `resign_date` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `resign_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resign`
+--
+
+LOCK TABLES `resign` WRITE;
+/*!40000 ALTER TABLE `resign` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transfer`
+--
+
+DROP TABLE IF EXISTS `transfer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transfer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `transfer_date` varchar(255) NOT NULL,
+  `former_department` varchar(255) NOT NULL,
+  `former_position` varchar(255) NOT NULL,
+  `current_department` varchar(255) NOT NULL,
+  `current_position` varchar(255) NOT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transfer`
+--
+
+LOCK TABLES `transfer` WRITE;
+/*!40000 ALTER TABLE `transfer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transfer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trial`
+--
+
+DROP TABLE IF EXISTS `trial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trial` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `begin_date` varchar(255) NOT NULL,
+  `end_date` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL,
+  `mark` varchar(255) NOT NULL DEFAULT '',
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trial`
+--
+
+LOCK TABLES `trial` WRITE;
+/*!40000 ALTER TABLE `trial` DISABLE KEYS */;
+INSERT INTO `trial` VALUES (1,'aaa','1','2017-01-21','2017-04-21','3个月','未通过',NULL),(2,'bbb','2','2017-05-30','2017-08-30','3个月','已录用',NULL),(3,'ccc','3','2017-03-10','2017-09-10','6个月','试用中',NULL);
+/*!40000 ALTER TABLE `trial` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'test','test'),(2,'啦啦啦','123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-21 18:09:43
---
+++ 第五组/公共文件/pms_database.sql
@@ -0,0 +1,189 @@
+﻿# Host: 127.0.0.1  (Version: 5.1.73-community)
+# Date: 2017-07-24 11:18:26
+# Generator: MySQL-Front 5.3  (Build 4.128)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "departments"
+#
+
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `mark` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "departments"
+#
+
+INSERT INTO `departments` VALUES (1,'生产部','001','using'),(2,'销售部','002','using'),(3,'财务部','003','using'),(6,'人事部','004','using'),(7,'采购部','005','delete'),(8,'行政部','006','using'),(11,'事业部','007','using'),(12,'技术部','008','delete');
+
+#
+# Structure for table "employee"
+#
+
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `sex` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `birthday` varchar(255) NOT NULL,
+  `id_number` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `entry_date` varchar(255) NOT NULL,
+  `work_date` varchar(255) NOT NULL,
+  `employment_form` varchar(255) NOT NULL,
+  `personnel_source` varchar(255) NOT NULL,
+  `statu` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "employee"
+#
+
+INSERT INTO `employee` VALUES (17,'王一','男','0001','2017-07-05','123','生产部','副经理','2017-07-06','2017-07-06','全日制用工','外部招聘','离职'),(18,'王二','男','0002','2017-07-04','123','事业部','经理','2017-07-05','2017-07-05','劳务派遣工','外部招聘','离职'),(19,'王三','男','0003','2017-07-15','123','行政部','经理','2017-07-16','2017-07-16','非全日制用工','内部提升','离职'),(20,'王四','男','0004','2017-07-04','123','行政部','副经理','2017-07-05','2017-07-05','全日制用工','内部提升','正式员工'),(21,'王五','男','0005','2017-07-04','123','生产部','经理','2017-07-05','2017-07-05','非全日制用工','内部提升','实习期'),(22,'王月','女','0006','2017-07-04','123','人事部','经理','2017-07-05','2017-07-05','非全日制用工','内部提升','正式员工'),(23,'李一','男','0007','2017-07-05','123','销售部','副经理','2017-07-06','2017-07-06','非全日制用工','内部提升','实习期'),(24,'李二','男','0008','2017-07-05','123','事业部','技术支持','2017-07-06','2017-07-06','全日制用工','外部招聘','实习期'),(25,'李三','女','0009','2017-07-05','123','生产部','技术支持','2017-07-06','2017-07-06','劳务派遣工','外部招聘','正式员工');
+
+#
+# Structure for table "people"
+#
+
+DROP TABLE IF EXISTS `people`;
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `marriage` varchar(255) NOT NULL DEFAULT '',
+  `folk` varchar(255) NOT NULL DEFAULT '',
+  `political` varchar(255) NOT NULL DEFAULT '',
+  `education` varchar(255) NOT NULL DEFAULT '',
+  `school` varchar(255) NOT NULL DEFAULT '',
+  `major` varchar(255) NOT NULL DEFAULT '',
+  `graduate_date` varchar(255) NOT NULL DEFAULT '',
+  `notes` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "people"
+#
+
+INSERT INTO `people` VALUES (14,'王一','0001','123','','','未婚','','','','','','',''),(15,'王二','0002','123','','','未婚','','','','','','',''),(16,'王三','0003','123','','','未婚','','','','','','',''),(17,'王四','0004','123','','','已婚','','','','','','',''),(18,'王五','0005','123','','','离异','','','','','','',''),(19,'王月','0006','123','','','未婚','','','','','','',''),(20,'李一','0007','123','','','已婚','','','','','','',''),(21,'李二','0008','123','','','未婚','','','','','','',''),(22,'李三','0009','123','','','已婚','','','','','','','');
+
+#
+# Structure for table "positions"
+#
+
+DROP TABLE IF EXISTS `positions`;
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `mark` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "positions"
+#
+
+INSERT INTO `positions` VALUES (1,'会计','01','财务部','using'),(2,'副经理','02','人事部','using'),(3,'技术支持','03','技术部','using'),(4,'经理','04','人事部','using'),(5,'产品经理','05','销售部','using'),(6,'总裁','06','人事部','using'),(7,'后台开发','07','事业部','using');
+
+#
+# Structure for table "resign"
+#
+
+DROP TABLE IF EXISTS `resign`;
+CREATE TABLE `resign` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `begin_date` varchar(255) NOT NULL,
+  `resign_date` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `resign_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "resign"
+#
+
+INSERT INTO `resign` VALUES (8,'王一','0001','2017-07-06','2017-07-18','财务部','会计','辞退'),(9,'王二','0002','2017-07-05','2017-07-18','生产部','技术支持','辞职'),(11,'王三','0003','2017-07-16','2017-07-23','行政部','经理','退休');
+
+#
+# Structure for table "transfer"
+#
+
+DROP TABLE IF EXISTS `transfer`;
+CREATE TABLE `transfer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `transfer_date` varchar(255) NOT NULL,
+  `former_department` varchar(255) NOT NULL,
+  `former_position` varchar(255) NOT NULL,
+  `current_department` varchar(255) NOT NULL,
+  `current_position` varchar(255) NOT NULL,
+  `notes` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "transfer"
+#
+
+INSERT INTO `transfer` VALUES (3,'王四','0004','2017-07-22','人事部','副经理','行政部','副经理',''),(4,'王五','0005','2017-07-22','财务部','经理','生产部','经理',''),(5,'王月','0006','2017-07-22','财务部','副经理','人事部','经理','');
+
+#
+# Structure for table "trial"
+#
+
+DROP TABLE IF EXISTS `trial`;
+CREATE TABLE `trial` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `begin_date` varchar(255) NOT NULL,
+  `end_date` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL,
+  `mark` varchar(255) NOT NULL DEFAULT '',
+  `notes` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "trial"
+#
+
+INSERT INTO `trial` VALUES (8,'王二','0002','2017-07-05','2017-10-05','3个月','using','已录用'),(9,'王三','0003','2017-07-16','2018-01-16','6个月','using','试用中'),(10,'王五','0005','2017-07-05','2017-10-05','3个月','using','试用中'),(11,'李一','0007','2017-07-06','2018-01-06','6个月','using','试用中'),(12,'李二','0008','2017-07-06','2017-10-06','3个月','using','试用中');
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
+INSERT INTO `users` VALUES (1,'test','test'),(2,'lalala','123'),(3,'admin','admin');
